1d56e29da48b6041fe1196e4ecd7df5be3a2e4a5
Stuart Schultz <stuart@35grove.com>
2012-05-21 15:59:11 -0400

    Updated controller spec to add new tests

--- lectures/05-rspec/5.1-rspec-refactor-exercises/spec/controllers/things_controller_spec.rb
+++ lectures/05-rspec/5.1-rspec-refactor-exercises/spec/controllers/things_controller_spec.rb
@@ -3,33 +3,49 @@ require 'spec_helper'
 describe ThingsController do
 
   context "with mocks" do
-
-    describe "GET index" do
-
+    describe "GET index" do #index
       let(:mock_thing) { mock_model(Thing).as_null_object }
-
       it "returns things" do
         Thing.stub!(:all).and_return [ mock_thing ]
         get :index
         assigns(:things).should eq [ mock_thing ]
       end
-
       it "returns things" do
         Thing.should_receive(:all)
         get :index
       end
-
-    end
-
-    describe "GET thing" do
-
+    end # index
+    describe "GET thing" do #show
       it "fetches thing from the database" do
         Thing.should_receive(:find).with("42").once
         get :show, id: 42
       end
-
-    end
-
-  end
+    end # describe show
+  end # context with mocks
+  
+  context "with fabricator" do
+    describe '#new' do #new
+      it "should display the new page" do
+        get :new
+        response.should be_success
+        assigns(:thing).should_not be_nil
+      end    
+    end # new
+    describe '#create' do #create
+      let(:fake_thing) {Fabricate.build(:thing)}
+      it "should create a thing" do
+        params = {thing: {name: fake_thing.name}}
+        expect {
+          post :create, params
+          response.should be_redirect
+        }.to change {Thing.count}.by(1)      
+      end
+    end # create
+    describe '#destroy' do
+      let(:fake_thing) {Fabricate.create(:thing)}
+      it "should destory a thing" do
+      end
+    end # destroy
+  end # context with fabricator
   
 end
